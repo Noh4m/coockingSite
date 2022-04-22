237893cb1421efc98ab9a3371356149099d9e9d5
Noham <72859612+Noh4m@users.noreply.github.com>
2022-04-22 12:03:07 +0200

    Map of the section Recipes of the day

--- src/Components/Card/CardRecipe.js
+++ src/Components/Card/CardRecipe.js
@@ -7,12 +7,12 @@ function CardRecipe(props) {
             <div className="CardRecipeContainer">
                 <div className="zoom">
                     <img className="CardRecipeImg" alt={props.recipe}
-                         src="https://images.unsplash.com/photo-1547592180-85f173990554?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
+                         src={props.img}/>
                 </div>
                 <h3>{props.recipe}</h3>
                 <div className="CardRecipeButton">
-                    <Button name="30 Minutes" icon="https://img.icons8.com/ios-glyphs/344/restaurant.png"/>
-                    <Button name="Chicken" icon="https://img.icons8.com/ios-glyphs/344/sport-stopwatch.png"/>
+                    <Button name={props.timeCooking} icon="https://img.icons8.com/ios-glyphs/344/restaurant.png"/>
+                    <Button name={props.categories} icon="https://img.icons8.com/ios-glyphs/344/sport-stopwatch.png"/>
                 </div>
             </div>
         </div>
--- src/Components/RecipesOfTheDay.js
+++ src/Components/RecipesOfTheDay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TitleAndSubTitle from "./TitleAndSubTitle";
 import CardRecipe from "./Card/CardRecipe";
+import {data} from "../Data/DataRecipes"
 
 function RecipesOfTheDay(props) {
     return (
@@ -8,8 +9,12 @@ function RecipesOfTheDay(props) {
             <TitleAndSubTitle title="Simple and tasty recipes"
                               subTitle="Lorem ipsum dolor sit amet, consectetuipisicing elit,
                               sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"/>
+            <div className="cardRecipes">
+                {data.slice(0, 4).map((data) =>(
+                    <CardRecipe key={data.id} recipe={data.nameRecipe} img={data.img}  categories={data.categories} timeCooking={data.timeCooking}/>
+                ))}
+            </div>
 
-            <CardRecipe recipe="Big and Juicy Wagyu Beef Cheeseburger"/>
 
         </div>
     );
